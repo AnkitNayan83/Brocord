395add60ac4dd297945102cc58f370e017d10139
AnkitNayan83 <ankitnayan83@gmail.com>
2023-11-30 11:30:09 +0530

    zustand

---
+++ components/UI Models/create-server-model.tsx
@@ -0,0 +1,128 @@
+"use client";
+
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+} from "@/components/ui/dialog";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import FileUpload from "./file-upload";
+import { useRouter } from "next/navigation";
+
+const formSchema = z.object({
+    name: z.string().min(1, {
+        message: "Server name is required",
+    }),
+    imageUrl: z.string().min(1, {
+        message: "Server image is required",
+    }),
+});
+
+const CreateServer = () => {
+    // to remove hydration error due to our form model
+    const router = useRouter();
+
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            imageUrl: "",
+        },
+    });
+
+    const isLoading = form.formState.isSubmitting;
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            await axios.post("/api/servers", values);
+            form.reset();
+            router.refresh();
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <Dialog open>
+            <DialogContent className="bg-white text-black p-0 overflow-hidden">
+                <DialogHeader className="pt-8 px-6">
+                    <DialogTitle className="text-2xl text-center font-bold">
+                        Create Your Server
+                    </DialogTitle>
+                    <DialogDescription className="text-zinc-500 text-center">
+                        Give your server a name and image. You can always change it later.
+                    </DialogDescription>
+                </DialogHeader>
+
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                        <div className="space-y-8 px-6">
+                            <div className="flex items-center justify-center text-center">
+                                <FormField
+                                    control={form.control}
+                                    name="imageUrl"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <FileUpload
+                                                    endpoint="serverImage"
+                                                    value={field.value}
+                                                    onChange={field.onChange}
+                                                />
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                />
+                            </div>
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                                            Server Name
+                                        </FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                disabled={isLoading}
+                                                className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                                                placeholder="Enter your server name"
+                                                {...field}
+                                            />
+                                        </FormControl>
+
+                                        {/* error message  */}
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <DialogFooter className="bg-gray-100 px-6 py-4">
+                            <Button variant={"primary"} disabled={isLoading}>
+                                Create
+                            </Button>
+                        </DialogFooter>
+                    </form>
+                </Form>
+            </DialogContent>
+        </Dialog>
+    );
+};
+
+export default CreateServer;
---
+++ components/navigation/navigation-item.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import Image from "next/image";
+import ActionTooltip from "./action-tooltip";
+import { cn } from "@/lib/utils";
+import { useParams, useRouter } from "next/navigation";
+
+interface NavigatioItemProps {
+    id: string;
+    imageUrl: string;
+    name: string;
+}
+
+const NavigatioItem = ({ id, imageUrl, name }: NavigatioItemProps) => {
+    const params = useParams();
+    const router = useRouter();
+
+    const handelClick = () => {
+        router.push(`/server/${id}`);
+    };
+
+    return (
+        <ActionTooltip side="right" align="center" label={name}>
+            <button onClick={handelClick} className="group relative flex items-center">
+                <div
+                    className={cn(
+                        "absolute left-0 bg-primary rounded-r-full transistion-all w-[4px]",
+                        params?.serverId !== id && "group-hover:h-[20px]",
+                        params?.serverId === id ? "h-[36px]" : "h-[8px]"
+                    )}
+                />
+                <div
+                    className={cn(
+                        "relative group flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transistion-all overflow-hidden",
+                        params?.serverId === id && "bg-primary/10 text-primary rounded-[16px]"
+                    )}
+                >
+                    <Image src={imageUrl} fill alt="server img" />
+                </div>
+            </button>
+        </ActionTooltip>
+    );
+};
+
+export default NavigatioItem;
