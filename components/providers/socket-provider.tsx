d26d06ebae764ff0c9b1e768eded143ab23a8394
AnkitNayan83 <ankitnayan83@gmail.com>
2023-12-12 18:51:11 +0530

    commit

---
+++ components/providers/socket-provider.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { io as ClientIO } from "socket.io-client";
+
+type SocketContextType = {
+    socket: any | null;
+    isConnected: boolean;
+};
+
+const SocketContext = createContext<SocketContextType>({
+    socket: null,
+    isConnected: false,
+});
+
+export const useSocket = () => {
+    return useContext(SocketContext);
+};
+
+export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
+    const [socket, setSocket] = useState(null);
+    const [isConnected, setIsConnected] = useState(false);
+};
